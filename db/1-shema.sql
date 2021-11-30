8442ec5964b83ff521becbf996c557f469695d7e
Yehor Bublyk <yehor.bk@gmail.com>
2021-11-30 23:19:17 +0200

    bugs with adding data fixed, docker-compose.yml updated, checking for cpu_count in schema fixed

--- db/1-shema.sql
+++ db/1-shema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS machines (
   id SERIAL PRIMARY KEY,
   name VARCHAR (256) NOT NULL,
   cpu_count SMALLINT NOT NULL
-    CHECK (cpu_count > 1),
+    CHECK (cpu_count >= 1),
   total_disk_space BIGINT NOT NULL DEFAULT 0
     CHECK (total_disk_space >= 0)
 );
--- db/2-data.sql
+++ db/2-data.sql
@@ -1,9 +1,9 @@
-INSERT INTO machines (id, name, cpu_count, total_disk_space) VALUES
-    (1, 'Ubuntu VM', 4, 17179869184),
-    (2, 'Windows VM', 2, 21474836480);
+INSERT INTO machines (name, cpu_count, total_disk_space) VALUES
+  ('Ubuntu VM', 4, 17179869184),
+  ('Windows VM', 2, 21474836480);
 
-INSERT INTO disks (id, machine_id, capacity) VALUES
-    (1, 1, 17179869184),
-    (2, 2, 17179869184),
-    (3, 2, 4294967296),
-    (4, NULL, 8589934592);
+INSERT INTO disks (machine_id, capacity) VALUES
+  (1, 17179869184),
+  (2, 17179869184),
+  (2, 4294967296),
+  (NULL, 8589934592);
