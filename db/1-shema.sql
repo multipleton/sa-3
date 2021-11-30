0f0666e07a62c53ef9011558f3a99549f2500a5b
Yehor Bublyk <yehor.bk@gmail.com>
2021-11-30 23:07:27 +0200

    schema and data sql files filled

--- db/1-shema.sql
+++ db/1-shema.sql
@@ -1,1 +1,17 @@
--- TODO
+CREATE TABLE IF NOT EXISTS machines (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (256) NOT NULL,
+  cpu_count SMALLINT NOT NULL
+    CHECK (cpu_count > 1),
+  total_disk_space BIGINT NOT NULL DEFAULT 0
+    CHECK (total_disk_space >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS disks (
+  id SERIAL PRIMARY KEY,
+  machine_id INTEGER,
+  capacity BIGINT NOT NULL
+    CHECK (capacity > 0),
+  FOREIGN KEY (machine_id) REFERENCES machines (id)
+    ON DELETE SET NULL
+);
--- db/2-data.sql
+++ db/2-data.sql
@@ -1,1 +1,9 @@
--- TODO
+INSERT INTO machines (id, name, cpu_count, total_disk_space) VALUES
+    (1, 'Ubuntu VM', 4, 17179869184),
+    (2, 'Windows VM', 2, 21474836480);
+
+INSERT INTO disks (id, machine_id, capacity) VALUES
+    (1, 1, 17179869184),
+    (2, 2, 17179869184),
+    (3, 2, 4294967296),
+    (4, NULL, 8589934592);
