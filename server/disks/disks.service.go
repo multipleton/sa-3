92518e5e4a21692fd0dde368e946b7af95317fda
Yehor Bublyk <yehor.bk@gmail.com>
2021-12-04 17:12:18 +0200

    disks service implemented

--- server/disks/disks.service.go
+++ server/disks/disks.service.go
@@ -1,1 +1,21 @@
 package disks
+
+type DisksService struct {
+	disksRepository DisksRepository
+}
+
+func (ds *DisksService) FindById(id uint32) (DisksEntity, error) {
+	result, err := ds.disksRepository.FindById(id)
+	return result, err
+}
+
+func (ds *DisksService) ConnectToMachine(id uint32, machineId uint32) (DisksEntity, error) {
+	var result DisksEntity
+	dbDisk, err := ds.FindById(id)
+	if err != nil {
+		return result, nil
+	}
+	dbDisk.MachineId = machineId
+	result, err = ds.disksRepository.Update(dbDisk)
+	return result, err
+}
