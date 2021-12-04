3cc4a10ca0ceb6c005d3bb9dd37f1bcf86c49667
Yehor Bublyk <yehor.bk@gmail.com>
2021-12-04 17:01:26 +0200

    typo fixed in disks repository

--- server/disks/disks.repository.go
+++ server/disks/disks.repository.go
@@ -8,20 +8,20 @@ type DisksRepository struct {
 	DB *sql.DB
 }
 
-func (dr *DisksRepository) findById(id uint32) (DisksEntity, error) {
+func (dr *DisksRepository) FindById(id uint32) (DisksEntity, error) {
 	var result DisksEntity
 	row := dr.DB.QueryRow("SELECT * FROM disks WHERE id=?", id)
 	err := row.Scan(&result.Id, &result.MachineId, &result.Capacity)
 	return result, err
 }
 
-func (dr *DisksRepository) update(entity DisksEntity) (DisksEntity, error) {
+func (dr *DisksRepository) Update(entity DisksEntity) (DisksEntity, error) {
 	var result DisksEntity
 	query := "UPDATE disks SET machine_id=? capacity=? WHERE id=?"
 	_, err := dr.DB.Exec(query, entity.MachineId, entity.Capacity, entity.Id)
 	if err != nil {
 		return result, err
 	}
-	result, err = dr.findById(entity.Id)
+	result, err = dr.FindById(entity.Id)
 	return result, err
 }
