d238acfecdcedb20107a056c2b3b3af37976a174
Yehor Bublyk <yehor.bk@gmail.com>
2021-12-04 17:00:09 +0200

    disks repository added

--- server/disks/disks.repository.go
+++ server/disks/disks.repository.go
@@ -1,1 +1,27 @@
 package disks
+
+import (
+	"database/sql"
+)
+
+type DisksRepository struct {
+	DB *sql.DB
+}
+
+func (dr *DisksRepository) findById(id uint32) (DisksEntity, error) {
+	var result DisksEntity
+	row := dr.DB.QueryRow("SELECT * FROM disks WHERE id=?", id)
+	err := row.Scan(&result.Id, &result.MachineId, &result.Capacity)
+	return result, err
+}
+
+func (dr *DisksRepository) update(entity DisksEntity) (DisksEntity, error) {
+	var result DisksEntity
+	query := "UPDATE disks SET machine_id=? capacity=? WHERE id=?"
+	_, err := dr.DB.Exec(query, entity.MachineId, entity.Capacity, entity.Id)
+	if err != nil {
+		return result, err
+	}
+	result, err = dr.findById(entity.Id)
+	return result, err
+}
