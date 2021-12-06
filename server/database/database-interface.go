bc53d1e522ec4e7257f507845623452675ea63d8
olegmak02 <55357151+olegmak02@users.noreply.github.com>
2021-12-06 22:45:46 +0200

    Update server/database/database-interface.go
    Co-authored-by: Yehor Bublyk <35728917+yehorbk@users.noreply.github.com>

--- server/database/database-interface.go
+++ server/database/database-interface.go
@@ -7,6 +7,7 @@ import (
 )
 
 func NewDatabaseConnection(host string, port string, user string, password string, dbname string, sslmode string) (*sql.DB, error) {
-	params := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
+	template := "host=%s port=%s user=%s password=%s dbname=%s sslmode=%s"
+	params := fmt.Sprintf(template, host, port, user, password, dbname, sslmode)
 	return sql.Open("postgres", params)
 }
