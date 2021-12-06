d88411e5000bf759a01bfc472a0a54a599ef4c90
Yehor Bublyk <yehor.bk@gmail.com>
2021-12-06 21:37:55 +0200

    functionality for updating machine with new totalAmountSpace added (NOTE: it doesn't tested)

--- server/disks/disks.service.go
+++ server/disks/disks.service.go
@@ -2,22 +2,48 @@ package disks
 
 type DisksService struct {
 	disksRepository *DisksRepository
+	machinesService *MachinesService
 }
 
-func (ds *DisksService) FindById(id uint32) (DisksEntity, error) {
+func (ds *DisksService) FindById(id uint32) (*DisksEntity, error) {
 	result, err := ds.disksRepository.FindById(id)
 	return result, err
 }
 
-func (ds *DisksService) ConnectToMachine(id uint32, dto DiskConnectToMachineDto) (DisksEntity, error) {
-	var result DisksEntity
+func (ds *DisksService) ConnectToMachine(id uint32, dto *DiskConnectToMachineDto) (*DisksEntity, error) {
+	var result *DisksEntity
 	dbDisk, err := ds.FindById(id)
 	if err != nil {
 		return result, nil
 	}
 	dbDisk.MachineId = dto.MachineId
 	result, err = ds.disksRepository.Update(dbDisk)
+	if err != nil {
+		return result, err
+	}
+	err = ds.updateMachineTotalDiskSpace(dto.MachineId)
+	if err != nil {
+		err = ds.disksRepository.Delete(id)
+	}
 	return result, err
+}
+
+func (ds *DisksService) updateMachineTotalDiskSpace(id uint32) error {
+	machine, err := ds.machinesService.FindById(id)
+	if err == nil {
+		return nil, err
+	}
+	disks, err := ds.disksRepository.FindByMachineId(id)
+	if err == nil {
+		return nil, err
+	}
+	var totalDiskSpace uint64
+	for _, disk := range disks {
+		totalDiskSpace += disk.Capacity
+	}
+	machine.TotalDiskSpace = totalDiskSpace
+	_, err = ds.machinesService.Update(id, machine)
+	return err
 }
 
 func NewDisksService(disksRepository *DisksRepository) *DisksService {
