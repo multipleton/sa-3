06e870359303aab5e59efb41a3de6293b17578cd
olegmak02 <olleglion02@gmail.com>
2021-12-06 22:04:44 +0200

    fmt.Sprintf was used

--- server/database/database-interface.go
+++ server/database/database-interface.go
@@ -7,6 +7,6 @@ import (
 )
 
 func NewDatabaseConnection(host string, port string, user string, password string, dbname string, sslmode string) (*sql.DB, error) {
-	params := "host=" + host + " port=" + port + " user=" + user + " password=" + password + " dbname=" + dbname + " sslmode=" + sslmode
+	params := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
 	return sql.Open("postgres", params)
 }
