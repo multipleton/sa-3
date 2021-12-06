511c7df5a9c2859908278d5eda1659a05e57d59d
olegmak02 <olleglion02@gmail.com>
2021-12-06 20:44:43 +0200

    Updated with new requirements

--- server/database/database-interface.go
+++ server/database/database-interface.go
@@ -6,23 +6,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type DatabaseInterface struct {
-	connection *sql.DB
-}
-
-func (d *DatabaseInterface) Init(host string, port string, user string, password string, dbname string, sslmode string) error {
+func NewDatabaseConnection(host string, port string, user string, password string, dbname string, sslmode string) (*sql.DB, error) {
 	connStr := "host=" + host + " port=" + port + " user=" + user + " password=" + password + " dbname=" + dbname + " sslmode=" + sslmode
-	var err error
-	d.connection, err = sql.Open("postgres", connStr)
-	return err
-}
-
-func (d *DatabaseInterface) GetConnection() *sql.DB {
-	return d.connection
-}
-
-func (d *DatabaseInterface) CloseConnection() {
-	if d.connection != nil {
-		d.connection.Close()
-	}
+	return sql.Open("postgres", connStr)
 }
